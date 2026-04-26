233d896d8654e5a80b8200fab1d4c0e0c6c6872f
cgzsqq <38458153+shiqingqi@users.noreply.github.com>
2026-04-26 22:59:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export default {
     const refererHost   = refererHeader ? new URL(refererHeader).hostname : '';
     const refererOrigin = refererHeader ? new URL(refererHeader).origin   : '';
     console.log(`refererHost: ${refererHost}, refererHeader: ${refererHeader}, refererOrigin: ${refererOrigin}`);
-    if (!refererHeader.includes('_cover') && !ALLOWED.has(refererHost) && ) {
+    if (!refererHeader.includes('_cover') && !ALLOWED.has(refererHost)) {
         console.log(`block, ${refererHeader}, refererHost: ${refererHost}`);
         return new Response('blocked', { status: 403 })
     } else {
